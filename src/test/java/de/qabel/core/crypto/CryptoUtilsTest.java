608e7686b231e7be9bbab4160d5f417dd46363b1
roeslpa <roesler@qabel.de>
2014-11-04 22:01:04 +0100

    removed unused imports

--- src/test/java/de/qabel/core/crypto/CryptoUtilsTest.java
+++ src/test/java/de/qabel/core/crypto/CryptoUtilsTest.java
@@ -4,10 +4,8 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
